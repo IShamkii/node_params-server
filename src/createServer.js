ffcb202e099839610f72597b9e16338900beaf1a
IShamkii <fanmetalheavy@gmail.com>
2025-02-24 23:50:16 +0200

    return alternative object

--- src/createServer.js
+++ src/createServer.js
@@ -24,3 +24,15 @@ function createServer() {
 module.exports = {
   createServer,
 };
+
+// eslint-disable-next-line no-unused-vars
+function expandedUrl(url) {
+  const pathName = url.pathname.slice(1).split('?')[0];
+  const parts = pathName ? pathName.split('/') : {};
+  const query = Object.fromEntries(url.searchParams.entries());
+
+  return {
+    ...(parts ? { parts } : {}),
+    ...(Object.keys(query).length ? { query } : {}),
+  };
+}
