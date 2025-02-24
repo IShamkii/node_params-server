993976719f0c6e554937d0c4d032ed85fd594dd2
IShamkii <fanmetalheavy@gmail.com>
2025-02-24 23:45:19 +0200

    add task solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,24 @@
-/* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const pathName = url.pathname.slice(1).split('?')[0];
+    const parts = pathName ? pathName.split('/') : {};
+    const query = Object.fromEntries(url.searchParams.entries());
+
+    res.setHeader('Content-Type', 'application/json');
+
+    try {
+      res.statusCode = 200;
+      res.end(JSON.stringify({ parts, query }));
+    } catch {
+      res.statusCode = 400;
+      res.end();
+    }
+  });
 }
 
 module.exports = {
